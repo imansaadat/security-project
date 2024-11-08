5b19293609fbc0c87f0e33445d41c072a222bcea
Iman Saadat <e.saadat72@gmail.com>
2024-11-09 02:41:47 +0330

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import Hash from "./components/Hash/Hash"
 
+
 function App() {
+  useEffect(() => {
+    document.title = "Encryption | Decryption";
+  }, []);
 
   return (
   <>
